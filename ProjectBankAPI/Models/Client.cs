ac4272e94206348cfb5b5e061db55827d49a9860
Physical8 <leomaje@gmail.com>
2025-01-30 04:53:55 -0500

    Prueba de Ramas

--- ProjectBankAPI/Models/Client.cs
+++ ProjectBankAPI/Models/Client.cs
@@ -8,7 +8,7 @@ namespace ProjectBankAPI.Models
         public required string Name { get; set; }
         public required string Email { get; set; }
 
-        // Relación con cuentas bancarias (Un cliente puede tener varias cuentas)
+        // Relación con cuentas bancarias (Un cliente puede tener varias cuentas) test git
 
         [JsonIgnore] // Evita la serialización circular
         public List<BankAccount> BankAccounts { get; set; } = new();
